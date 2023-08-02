0f6c6b8133f93c8186b8c361954a332e0671a41f
Jakub Matuszak <jaqobgm@gmail.com>
2023-08-02 09:38:25 +0200

    Implemented deltaTime + better Randomizer of colors

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <vector>
+#include <random>
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
@@ -12,14 +13,14 @@ class GravitySource
 public:
     GravitySource(float pos_x, float pos_y, float strength)
     {
-        this->pos.x = pos_x;
-        this->pos.y = pos_y;
+        this->pos.x = pos_x - 25;
+        this->pos.y = pos_y - 25;
 
         this->strength = strength;
 
         circleShape.setPosition(pos);
         circleShape.setFillColor(sf::Color::White);
-        circleShape.setRadius(10);
+        circleShape.setRadius(50);
     }
 
     void render(sf::RenderWindow& window)
@@ -45,7 +46,7 @@ class Particle
     sf::CircleShape circleShape;
 
 public:
-    Particle(float pos_x, float pos_y, float vel_x, float vel_y)
+    Particle(float pos_x, float pos_y, float vel_x, float vel_y, sf::Color color)
     {
         this->pos.x = pos_x;
         this->pos.y = pos_y;
@@ -54,7 +55,7 @@ public:
         this->vel.y = vel_y;
 
         circleShape.setPosition(pos);
-        circleShape.setFillColor(sf::Color::White);
+        circleShape.setFillColor(color);
         circleShape.setRadius(5);
     }
 
@@ -64,17 +65,12 @@ public:
         window.draw(circleShape);
     }
 
-    void setColor(sf::Color& color)
-    {
-        circleShape.setFillColor(color);
-    }
-
     sf::Vector2f getPos()
     {
         return pos;
     }
 
-    void updatePhysics(GravitySource& gravitySource)
+    void updatePhysics(GravitySource& gravitySource, float deltaTime)
     {
         float distanceX = gravitySource.getPos().x - pos.x;
         float distanceY = gravitySource.getPos().y - pos.y;
@@ -88,8 +84,8 @@ public:
 
         float inverseSquareDropoff = inverseDistance * inverseDistance;
 
-        float accelerationX = normalizedX * gravitySource.getStrength() * inverseSquareDropoff;
-        float accelerationY = normalizedY * gravitySource.getStrength() * inverseSquareDropoff;
+        float accelerationX = normalizedX * gravitySource.getStrength() * inverseSquareDropoff * deltaTime;
+        float accelerationY = normalizedY * gravitySource.getStrength() * inverseSquareDropoff * deltaTime;
 
         vel.x += accelerationX;
         vel.y += accelerationY;
@@ -99,53 +95,46 @@ public:
     }
 };
 
-sf::Color valToColor(float value)
-{
-    if (value < 0.0f) value = 0;
-    if (value > 1.0f) value = 1;
-
-    int r = 0, g, b = 0;
-
-    if (value < 0.5f)
-    {
-        b = (int)(255 * (1.0f - 2 * value));
-        g = (int)(255 * 2 * value);
-    }
-    else
-    {
-        g = (int)(255 * (1.0f - 2 * value));
-        r = (int)(255 * 2 * value);
-    }
-
-    return sf::Color(r, g, b);
-}
-
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1600, 1000), "Gravity Simulator");
+    //Using new random implemented in C++11
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> randColor(0, 256);
+    std::uniform_real_distribution<float> randPosX(0, 1600);
+    std::uniform_real_distribution<float> randPosY(0, 1000);
+    std::uniform_real_distribution<float> randVel(0, 1);
+
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+
+    sf::RenderWindow window(sf::VideoMode(1600, 1000), "Gravity Simulator", sf::Style::Close | sf::Style::Titlebar, settings);
     window.setFramerateLimit(120);
 
     std::cout << "Window Initialized" << std::endl;
 
+    // Earth - 6e24, Moon - 7.3e22, Sun - 2e30
+
     std::vector<GravitySource> gravitySources;
-    gravitySources.push_back(GravitySource(500, 500, 7000));
-    gravitySources.push_back(GravitySource(1200, 500, 7000));
+    gravitySources.push_back(GravitySource(800, 500, 0x6e24));
+    //gravitySources.push_back(GravitySource(500, 500, 500000));
+    //gravitySources.push_back(GravitySource(1200, 500, 500000));
 
-    int particlesNum = 2000;
-
+    int particlesNum = 20;
     std::vector<Particle> particles;
 
     for (int i=0; i<particlesNum; i++)
     {
-        particles.push_back(Particle(600, 700, 4, (float)(0.2f + (0.1 / particlesNum) * i)));
-
-        sf::Color color = valToColor((float)i / (float)particlesNum);
-
-        particles[i].setColor(color);
+        particles.push_back(Particle(randPosX(mt), randPosY(mt), randVel(mt), randVel(mt), sf::Color(randColor(mt), randColor(mt), randColor(mt))));
+        //particles.push_back(Particle(600, 700, 4, (float)(0.2f + (0.1 / particlesNum) * i), sf::Color(dist(mt), dist(mt), dist(mt))));
     }
+
+    sf::Clock deltaClock;
 
     while (window.isOpen())
     {
+        sf::Time deltaTime = deltaClock.restart();
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -160,7 +149,7 @@ int main()
         {
             for (auto & particle : particles)
             {
-                particle.updatePhysics(gravitySource);
+                particle.updatePhysics(gravitySource, deltaTime.asSeconds());
             }
         }
 
@@ -175,7 +164,7 @@ int main()
             if ((particle.getPos().x <= 1600 && particle.getPos().y <= 1000) || (particle.getPos().x >= 0 && particle.getPos().y >= 0)) count++;
         }
 
-        std::cout << "Currently visible particles on screen: " << count << std::endl;
+        //std::cout << "Currently visible particles on screen: " << count << std::endl;
 
         window.display();
     }
