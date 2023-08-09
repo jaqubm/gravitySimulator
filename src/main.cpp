ea4a3c9e894203b06f53bcfbd7969543622ae97b
jaqubm <jaqobgm@gmail.com>
2023-08-09 14:24:58 +0200

    FPS Independency of Simulation

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,6 @@
 
 //TODO: Create Scene class to run the simulation
 
-//TODO: Find better solution to make simulation FPS independent
-
 int main()
 {
     //Using new random implemented in C++11
@@ -59,7 +57,7 @@ int main()
     gravitySources.emplace_back(W * .5f + 300, H * .5f, 5 * 0x6e24, 30);    //Earth
 
     //Creating Particles
-    int particlesNum = 1;
+    int particlesNum = 1000;
     std::vector<Particle> particles;
     particles.reserve(particlesNum);
 
