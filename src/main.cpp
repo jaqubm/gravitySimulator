2074b74ef5402252c73030ae2b7ce5316a7597dc
jaqubm <jaqobgm@gmail.com>
2023-08-10 21:45:50 +0200

    Delete TODO

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 //TODO: Create more advanced collision detection
 //TODO: Create more advanced collision response
 
-//TODO: Add Functions to implement different Scenes
-
 int main()
 {
     Scene scene;
