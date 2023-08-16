3b85549080bac997ccd90c5a53ecafcfcb60a5ab
jaqubm <jaqobgm@gmail.com>
2023-08-16 14:11:08 +0200

    TODO

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 //TODO: Add comments describing code
 
+//TODO: Handle situation where velocity vector doesn't collide with gravitySource but Particle does it anyway
+
 int main()
 {
     Scene scene;
